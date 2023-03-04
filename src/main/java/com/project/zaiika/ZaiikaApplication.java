91bd74533ed267b0c5e8893e7292c8a0d0279986
Arseny <20mineslend14@gmail.com>
2023-03-04 16:42:58 +0500

    add rsa key

--- src/main/java/com/project/zaiika/ZaiikaApplication.java
+++ src/main/java/com/project/zaiika/ZaiikaApplication.java
@@ -1,8 +1,11 @@
 package com.project.zaiika;
 
+import com.project.zaiika.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ZaiikaApplication {
 
--- src/main/java/com/project/zaiika/config/RsaKeyProperties.java
+++ src/main/java/com/project/zaiika/config/RsaKeyProperties.java
@@ -1,2 +1,10 @@
-package com.project.zaiika.config;public class RsaKeyProperties {
+package com.project.zaiika.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 }
