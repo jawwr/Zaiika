ff885f8b8339fdec781d5ad7d4751210d03ee651
Arseny <20mineslend14@gmail.com>
2023-04-29 15:01:36 +0500

    create gateway and eureka server

---
+++ Gateway/src/test/java/com/zaiika/gateway/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaiika.gateway.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
