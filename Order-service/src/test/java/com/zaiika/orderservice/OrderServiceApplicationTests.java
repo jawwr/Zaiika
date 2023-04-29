318ecd5db8d1e78b866d1050c9ef4966260b3278
Arseny <20mineslend14@gmail.com>
2023-04-29 15:44:41 +0500

    eureka server with gateway

---
+++ Order-service/src/test/java/com/zaiika/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaiika.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
